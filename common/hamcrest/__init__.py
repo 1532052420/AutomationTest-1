5625fa01094ae5e317e1ec89584974d8b6d306ba
1532052420 <42493125+1532052420@users.noreply.github.com>
2022-11-09 17:10:06 +0800

    更新断言方法

---
+++ common/hamcrest/__init__.py
@@ -0,0 +1,9 @@
+#
+# __init__.py
+# @author 1532052420
+# @description 
+# @github https://github.com/1532052420
+# @created 2022-11-08T18:13:05.752Z+08:00
+# @last-modified 2022-11-08T18:13:08.469Z+08:00
+#
+

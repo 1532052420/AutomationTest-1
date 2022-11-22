34545c0e8d1ddc3766d658eac0cea670b1255ff7
1532052420 <42493125+1532052420@users.noreply.github.com>
2022-11-22 15:39:56 +0800

    remove monkey

--- cases/app_ui_mokey/android/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# __init__.py
-# @author 1532052420
-# @description 
-# @created 2021-05-18T20:52:28.612Z+08:00
-# @last-modified 2021-05-20T18:03:35.431Z+08:00
-# github https://github.com/1532052420
-
